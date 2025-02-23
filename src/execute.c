6d88b7be11070ae88ed983a080e018d65a559b77
Raffaele Cannarsa <rcannars@e3r7p1.42roma.it>
2025-02-23 14:28:25 +0100

    pipex finito

--- src/execute.c
+++ src/execute.c
@@ -6,13 +6,82 @@
 /*   By: rcannars <rcannars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:00:13 by rcannars          #+#    #+#             */
-/*   Updated: 2025/02/10 13:37:09 by rcannars         ###   ########.fr       */
+/*   Updated: 2025/02/23 14:25:53 by rcannars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-static void	child_process(t_pipex *pipex, int i, char **envp)
+static void	setup_child_pipes(t_pipex *pipex, int i)
+{
+	if (i == 0)
+	{
+		dup2(pipex->infile, STDIN_FILENO);
+		dup2(pipex->pipe[1], STDOUT_FILENO);
+	}
+	else if (i == pipex->cmd_count - 1)
+	{
+		dup2(pipex->pipe[2 * i - 2], STDIN_FILENO);
+		dup2(pipex->outfile, STDOUT_FILENO);
+	}
+	else
+	{
+		dup2(pipex->pipe[2 * i - 2], STDIN_FILENO);
+		dup2(pipex->pipe[2 * i + 1], STDOUT_FILENO);
+	}
+}
+
+static void	close_all_pipes(t_pipex *pipex)
+{
+	int	j;
+
+	j = 0;
+	while (j < (pipex->cmd_count - 1) * 2)
+	{
+		close(pipex->pipe[j]);
+		j++;
+	}
+	close(pipex->infile);
+	close(pipex->outfile);
+}
+
+void	child_process(t_pipex *pipex, int i, char **envp)
+{
+	setup_child_pipes(pipex, i);
+	close_all_pipes(pipex);
+	execve(pipex->cmd_paths[i], pipex->cmd_args[i], envp);
+	error_exit("Command execution failed");
+	exit(127);
+}
+
+static void	wait_children(t_pipex *pipex)
+{
+	int	i;
+	int	status;
+	int	exit_status;
+
+	i = 0;
+	while (i < pipex->cmd_count)
+	{
+		waitpid(pipex->pids[i], &status, 0);
+		if (WIFEXITED(status))
+		{
+			exit_status = WEXITSTATUS(status);
+			if (exit_status != 0 && exit_status != 1)
+				ft_printf("Command %d failed with status %d\n", i, exit_status);
+		}
+		i++;
+	}
+}
+
+void	execute_commands(t_pipex *pipex, char **envp)
+{
+	create_pipes(pipex);
+	create_children(pipex, envp);
+	close_all_pipes(pipex);
+	wait_children(pipex);
+}
+/* static void	child_process(t_pipex *pipex, int i, char **envp)
 {
 	int	j;
 
@@ -44,10 +113,48 @@ static void	child_process(t_pipex *pipex, int i, char **envp)
 	exit(127);
 }
 
-/* void	execute_commands(t_pipex *pipex, char **envp)
+void	execute_commands(t_pipex *pipex, char **envp)
 {
 	int	i;
+	int	status;
+	int	exit_status;
+	int	i;
 
+	i = 0;
+	while (i < pipex->cmd_count - 1)
+	{
+		if (pipe(pipex->pipe + 2 * i) < 0)
+			error_exit("Pipe creation failed");
+		i++;
+	}
+	i = 0;
+	while (i < pipex->cmd_count)
+	{
+		pipex->pids[i] = fork();
+		if (pipex->pids[i] < 0)
+			error_exit("Fork failed");
+		if (pipex->pids[i] == 0)
+			child_process(pipex, i, envp);
+		i++;
+	}
+	i = 0;
+	while (i < (pipex->cmd_count - 1) * 2)
+		close(pipex->pipe[i++]);
+	i = 0;
+	while (i < pipex->cmd_count)
+	{
+		waitpid(pipex->pids[i], &status, 0);
+		if (WIFEXITED(status))
+		{
+			exit_status = WEXITSTATUS(status);
+			if (exit_status != 0 && exit_status != 1)
+				ft_printf("Command %d failed with status %d\n", i, exit_status);
+		}
+		i++;
+	}
+} */
+/* void	execute_commands(t_pipex *pipex, char **envp)
+{
 	i = 0;
 	while (i < pipex->cmd_count - 1)
 	{
@@ -79,49 +186,3 @@ static void	child_process(t_pipex *pipex, int i, char **envp)
 	}
 }
  */
-
- void	execute_commands(t_pipex *pipex, char **envp)
-{
-	int	i;
-	int	status;
-
-	// Creazione delle pipe
-	i = 0;
-	while (i < pipex->cmd_count - 1)
-	{
-		if (pipe(pipex->pipe + 2 * i) < 0)
-			error_exit("Pipe creation failed");
-		i++;
-	}
-
-	// Fork dei processi
-	i = 0;
-	while (i < pipex->cmd_count)
-	{
-		pipex->pids[i] = fork();
-		if (pipex->pids[i] < 0)
-			error_exit("Fork failed");
-		if (pipex->pids[i] == 0)
-			child_process(pipex, i, envp);
-		i++;
-	}
-
-	// Chiudere tutte le pipe nel processo padre
-	i = 0;
-	while (i < (pipex->cmd_count - 1) * 2)
-		close(pipex->pipe[i++]);
-
-	// Attendere i processi figli e gestire gli exit status
-	i = 0;
-	while (i < pipex->cmd_count)
-	{
-		waitpid(pipex->pids[i], &status, 0);
-		if (WIFEXITED(status))
-		{
-			int exit_status = WEXITSTATUS(status);
-			if (exit_status != 0 && exit_status != 1) // Ignora exit status 1
-				ft_printf("Command %d failed with status %d\n", i, exit_status);
-		}
-		i++;
-	}
-}
--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: rcannars <rcannars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:00:21 by rcannars          #+#    #+#             */
-/*   Updated: 2025/02/21 14:36:17 by rcannars         ###   ########.fr       */
+/*   Updated: 2025/02/23 14:27:49 by rcannars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,66 @@ static char	*get_cmd_path(char *cmd, char **envp)
 	return (ft_strdup(cmd));
 }
 
+void	handle_files(t_pipex *pipex, int argc, char **argv)
+{
+	if (pipex->here_doc)
+	{
+		handle_here_doc(pipex, pipex->limiter);
+		pipex->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND,
+				0644);
+	}
+	else
+	{
+		pipex->infile = open(argv[1], O_RDONLY);
+		if (pipex->infile < 0)
+		{
+			ft_putstr_fd("Input file error\n", 2);
+			argv[1] = NULL;
+			argv[2] = "echo -n";
+		}
+		pipex->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC,
+				0644);
+	}
+	if (pipex->outfile < 0)
+	{
+		error_exit("Output file error");
+		exit(1);
+	}
+}
+
+void	process_command(t_pipex *pipex, char **argv, char **envp, int index)
+{
+	int	start;
+
+	if (pipex->here_doc)
+		start = 3;
+	else
+		start = 2;
+	pipex->cmd_args[index] = ft_split(argv[index + start], ' ');
+	if (!pipex->cmd_args[index] || !pipex->cmd_args[index][0])
+	{
+		ft_putstr_fd("Empty command\n", 2);
+		exit(1);
+	}
+	pipex->cmd_paths[index] = get_cmd_path(pipex->cmd_args[index][0], envp);
+}
+
 void	parse_args(t_pipex *pipex, int argc, char **argv, char **envp)
+{
+	int	i;
+
+	handle_files(pipex, argc, argv);
+	i = 0;
+	while (i < pipex->cmd_count)
+	{
+		process_command(pipex, argv, envp, i);
+		i++;
+	}
+	pipex->cmd_args[i] = NULL;
+	pipex->cmd_paths[i] = NULL;
+}
+
+/* void	parse_args(t_pipex *pipex, int argc, char **argv, char **envp)
 {
 	int	i;
 	int	start;
@@ -61,8 +120,6 @@ void	parse_args(t_pipex *pipex, int argc, char **argv, char **envp)
 			ft_putstr_fd("Input file error\n", 2);
 			argv[1] = NULL;
 			argv[2] = "echo -n";
-			//error_exit(char *msg);
-			//exit(1);
 		}
 	}
 	if (pipex->here_doc)
@@ -89,4 +146,4 @@ void	parse_args(t_pipex *pipex, int argc, char **argv, char **envp)
 	}
 	pipex->cmd_args[i] = NULL;
 	pipex->cmd_paths[i] = NULL;
-}
+} */
